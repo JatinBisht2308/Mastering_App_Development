be832042ada37895d3f2dd01a410c91ef3929589
JatinBisht2308 <jatinbisht2308@gmail.com>
2023-04-29 14:20:26 +0530

    :fire: created the basic structure of the home page

--- fmj/app/index.js
+++ fmj/app/index.js
@@ -1,10 +1,46 @@
-import { View, Text } from 'react-native';
+import { View, ScrollView, SafeAreaView, Text } from "react-native";
+import { useState } from "react";
+import { Stack, useRouter } from "expo-router";
+// import { Screen } from "expo-router/build/views/Screen";
+import { COLORS, icons, images, SIZES } from "../constants";
+import {
+  Nearbyjobs,
+  Popularjobs,
+  ScreenHeaderBtn,
+  Welcome,
+} from "../components";
 
-const Home = () =>{
-    return(
-      <View>
-        <Text>Home</Text>
-      </View>
-    );
-}
+
+const Home = () => {
+  const router = useRouter();
+  return (
+    <SafeAreaView style={{ flex: 1, backgroundColor: COLORS.lightWhite }}>
+      <Stack.Screen
+        options={{
+          headerStyle: { backgroundColor: COLORS.lightWhite },
+          headerShadowVisible: false,
+          headerLeft: () => (
+            <ScreenHeaderBtn iconUrl={icons.menu} dimension="60%" />
+          ),
+          headerRight : () =>(
+            <ScreenHeaderBtn iconUrl={icons.profile} dimension="100%" />
+          ),
+          headerTitle: ""
+        }}
+      />
+      <ScrollView showsVerticalScrollIndicator={false}>
+        <View
+        style={{
+            flex:1,
+            padding: SIZES.medium,
+        }}
+        >
+            <Welcome/>
+            <Popularjobs/>
+            <Nearbyjobs/>
+        </View>
+      </ScrollView>
+    </SafeAreaView>
+  );
+};
 export default Home;
