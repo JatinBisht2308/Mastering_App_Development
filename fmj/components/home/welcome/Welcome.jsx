cb1388404967cf6623def16399f3211d1bf8882f
JatinBisht2308 <jatinbisht2308@gmail.com>
2023-04-29 16:45:27 +0530

    :fire: ui of welcome is completed

--- fmj/components/home/welcome/Welcome.jsx
+++ fmj/components/home/welcome/Welcome.jsx
@@ -11,8 +11,10 @@ import { useRouter } from "expo-router";
 import styles from "./welcome.style";
 import { icons, SIZES } from "../../../constants";
 
+const jobsTypes = ["Full-time", "Part-time", "Contracting"];
 const Welcome = () => {
   const router = useRouter();
+  const [activeJobType, setActiveJobType] = useState(jobsTypes[0]);
   return (
     <View>
       <View style={styles.container}>
@@ -20,11 +22,43 @@ const Welcome = () => {
         <Text style={styles.welcomeMessage}>Find your perfect job</Text>
       </View>
       <View style={styles.searchContainer}>
-         <View style={styles.searchWrapper}>
+        <View style={styles.searchWrapper}>
           <TextInput
-          style={styles.searchInput}
+            style={styles.searchInput}
+            value=""
+            onChange={() => {}}
+            placeholder="What are you looking for?"
           />
-         </View>
+        </View>
+        <TouchableOpacity style={styles.searchBtn} onPress={() => {}}>
+          <Image
+            source={icons.search}
+            resizeMode="contain"
+            style={styles.searchBtnImage}
+          />
+        </TouchableOpacity>
+      </View>
+
+      {/* for flat list */}
+      <View style={styles.tabsContainer}>
+        <FlatList
+          data={jobsTypes}
+          renderItem={({ item }) => (
+            <TouchableOpacity
+              style={styles.tab(activeJobType, item)}
+              onPress={() => {
+                setActiveJobType(item);
+                router.push("/search/${item");
+              }}
+            >
+              <Text style={styles.tabText(activeJobType, item)}>{item}</Text>
+            </TouchableOpacity>
+          )}
+          keyExtractor={item => item}
+          contentContainerStyle={{columnGap: SIZES.small}}
+          horizontal
+          showsHorizontalScrollIndicator={false}
+        />
       </View>
     </View>
   );
